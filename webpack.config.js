6960d8db16707bec4ca0026bde6894c8cac770ba
Ann Antonova <ms.antanol@yandex.ru>
2021-05-16 22:33:58 +0300

    Init commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const webpack = require('webpack');
+
+/*
+    entry — файл, с которого начинается сборка приложения. Их может быть несколько, тогда и сборка будет не одна;
+    context — папка, в которой нужно искать entry;
+    output содержит имя файла сборки (filename) и путь до папки, куда этот файл будет сохранен (path).
+
+    __dirname — это константа node.js, определяемая автоматически в каждом файле и содержащая путь до папки, в которой лежит исполняемый файл 
+                (у нас это корневая папка).
+*/
+
+module.exports = {
+   entry: {
+       app: './index.js',
+   },
+   context: path.resolve(__dirname, 'static_src'),
+   output: {
+       path: path.resolve(__dirname, 'static', 'build'),
+       filename: 'app.js'
+   },
+
+   module: {
+    rules: [
+        {
+            test: /\.(js|jsx)$/,
+            include: path.resolve(__dirname, "static_src"),
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            }
+        }
+    ]
+   }
+};
