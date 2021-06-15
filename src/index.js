a96dd51ed3a58f00a818064f5664a3233fd00cb3
Ann Antonova <ms.antanol@yandex.ru>
2021-06-15 16:29:05 +0300

    Подключила redux-persist

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { CssBaseline } from '@material-ui/core';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 
 import './index.scss';
 import Router from './components/Layout/Router';
-import { store } from './store';
+import { persistor, store } from './store';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <CssBaseline />
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+  <Provider store={ store }>
+    <PersistGate persistor={ persistor } loading = { null }>
+      <CssBaseline />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
--- src/store.js
+++ src/store.js
@@ -1,18 +1,30 @@
 import { applyMiddleware, createStore, combineReducers, compose } from 'redux';
-import {profileReducer} from './reducers/profile';
-import {messagesReducer} from './reducers/chat';
+import { persistStore, persistReducer } from 'redux-persist'
 import thunk from 'redux-thunk';
 
-const rootReduser = combineReducers({
+import storage from 'redux-persist/lib/storage';
+
+import { profileReducer } from './reducers/profile';
+import { messagesReducer } from './reducers/chat';
+
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+const rootReducer = combineReducers({
     profile: profileReducer,
     chats: messagesReducer
 });
 
-// для Redux панели в инструментах разбаботчика вместо
-// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
-    rootReduser,
+    persistedReducer,
     composeEnhancers(applyMiddleware(thunk))
 );
+
+export const persistor = persistStore(store);
